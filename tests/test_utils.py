3a6eacfc517e801ff5234f7d6f473b13149ddc97
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-05 12:49:46 +0200

    fix: removed suffix from name sanitization

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -123,15 +123,15 @@ def _setup_db(tmp_path):
 def test_sanitize():
     assert utils.sanitize("good") == "good"
     assert utils.sanitize("with-dash") == "with_dash"
-    assert utils.sanitize('$%_()') == '_____'
+    assert utils.sanitize('$%_()') == '____'
     assert utils.sanitize('UPPER') == 'UPPER'
     assert utils.sanitize('numb3r') == 'numb3r'
-    assert utils.sanitize('newline\n') == 'newline_'
-    assert utils.sanitize('newline\r') == 'newline_'
+    assert utils.sanitize('newline\n') == 'newline'
+    assert utils.sanitize('newline\r') == 'newline'
 
 def test_get_id():
     assert utils.get_id('no-good') == 'ns_no_good'
-    assert utils.get_id('nospace ') == 'ns_nospace_'
+    assert utils.get_id('nospace ') == 'ns_nospace'
     assert utils.get_id('t1234') == 'ns_t1234'
     # str with 97 chars
     long_str = "ihTSEf2Y5rl8TX96pWFFPMty9LFgH3GezhVueGoDB6_aaIFhDSKe1ZR64cV41iSVhfrm5wJCUPFfMGx2fBZyhDIW9cl9SCI43"
