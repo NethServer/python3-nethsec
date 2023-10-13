3419f7a40ffb4d41c50550beef883b4da353bc43
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-13 12:14:15 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'nethsec',
-    version = '0.0.8',
+    version = '0.0.9',
     author = 'Giacomo Sanchietti',
     author_email = 'giacomo.sanchietti@nethesis.it',
     description = 'Utilities for NethSecurity development',
