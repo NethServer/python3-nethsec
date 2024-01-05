463efd121fc852d10f05ea83079b4b146ee5dc77
Andrea Leardini <andre8244@gmail.com>
2024-01-05 09:32:14 +0100

    Bump release to 0.0.30

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'nethsec',
-    version = '0.0.29',
+    version = '0.0.30',
     author = 'Giacomo Sanchietti',
     author_email = 'giacomo.sanchietti@nethesis.it',
     description = 'Utilities for NethSecurity development',
