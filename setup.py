31a4d5300b471c47e7a5e9aa7bcf83e316ee26a6
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-05-17 13:10:01 +0200

    chore: bump release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'nethsec',
-    version = '0.0.54',
+    version = '0.0.55',
     author = 'Giacomo Sanchietti',
     author_email = 'giacomo.sanchietti@nethesis.it',
     description = 'Utilities for NethSecurity development',
