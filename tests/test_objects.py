ee1b94d7d13d4ca89afa7c800bebe80211758511
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-05-30 09:34:49 +0200

    objects: tests, use fixture

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,8 +1,7 @@
-from nethsec import utils
+import pathlib
 from nethsec.utils import ValidationError
 import pytest
 from euci import EUci, UciExceptionNotFound
-
 from nethsec import firewall, objects
 
 objects_db = """
@@ -55,8 +54,18 @@ config user 'ns_user2'
 	option name "user2"
 """
 
-def _setup_db(tmp_path):
-     # setup fake dbs
+mwan3_db = """
+"""
+
+dpi_db = """
+"""
+
+@pytest.fixture
+def u(tmp_path: pathlib.Path) -> EUci:
+    conf_dir = tmp_path.joinpath('conf')
+    conf_dir.mkdir()
+    save_dir = tmp_path.joinpath('save')
+    save_dir.mkdir()
     with tmp_path.joinpath('objects').open('w') as fp:
         fp.write(objects_db)
     with tmp_path.joinpath('firewall').open('w') as fp:
@@ -65,10 +74,13 @@ def _setup_db(tmp_path):
         fp.write(dhcp_db)
     with tmp_path.joinpath('users').open('w') as fp:
         fp.write(user_db)
+    with tmp_path.joinpath('mwan3').open('w') as fp:
+        fp.write(mwan3_db)
+    with tmp_path.joinpath('dpi').open('w') as fp:
+        fp.write(dpi_db)
     return EUci(confdir=tmp_path.as_posix())
 
-def test_add_doman_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_add_doman_set(u):
     id1 = objects.add_domain_set(u, "mydomainset", "ipv4", ["test1.com", "test2.com"])
     assert u.get("objects", id1, "name") == "mydomainset"
     assert u.get("objects", id1, "family") == "ipv4"
@@ -96,8 +108,7 @@ def test_add_doman_set(tmp_path):
     assert u.get('firewall', linked['firewall'][0], 'name') == "mydomainset2"
     assert u.get_all('dhcp', linked['dhcp'][0], 'name') == ("mydomainset2",)
 
-def test_edit_domain_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_edit_domain_set(u):
     id = objects.add_domain_set(u, "mydomainset3", "ipv4", ["test1.com", "test2.com"])
     objects.edit_domain_set(u, id, "mydomainset3b", "ipv6", ["test3.com", "test4.com"], 600)
     assert u.get("objects", id, "name") == "mydomainset3b"
@@ -105,8 +116,7 @@ def test_edit_domain_set(tmp_path):
     assert u.get_all("objects", id, "domain") == ("test3.com", "test4.com")
     assert u.get("objects", id, "timeout") == "600"
     
-def test_delete_domain_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_delete_domain_set(u):
     with pytest.raises(ValidationError):
         objects.delete_domain_set(u, "notpresent")
     id = objects.add_domain_set(u, "mydomainset4", "ipv4", ["test1.com", "test2.com"])
@@ -115,21 +125,18 @@ def test_delete_domain_set(tmp_path):
     assert linked['firewall'] == []
     assert linked['dhcp'] == []
 
-def test_is_used_domain_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_used_domain_set(u):
     id = objects.add_domain_set(u, "used1", "ipv4", ["test1.com", "test2.com"])
     u.set('firewall', 'r5', 'ns_dst', f"objects/{id}")
     used, matches = objects.is_used_domain_set(u, id)
     assert used
     assert matches == ["firewall/r5"]
 
-def test_list_domain_sets(tmp_path):
-    u = _setup_db(tmp_path)
+def test_list_domain_sets(u):
     sets = objects.list_domain_sets(u)
     assert len(sets) == 6
 
-def test_add_host_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_add_host_set(u):
     with pytest.raises(ValidationError):
         objects.add_host_set(u, "myhostset", "ipv4", ["a.b.c.d", "e.f.g.h"])
     id1 = objects.add_host_set(u, "myhostset", "ipv4", ["1.2.3.4", "4.5.6.0/24", "192.168.1.3-192.168.1.10"])
@@ -141,77 +148,65 @@ def test_add_host_set(tmp_path):
     assert u.get("objects", id2, "family") == "ipv6"
     assert u.get_all("objects", id2, "ipaddr") == ("2001:db8:3333:4444:5555:6666:7777:8888", "2001:db8::/95", "2001:db8:3333:4444:5555:6666:7777:8888-2001:db8:3333:4444:5555:6666:7777:8890")
 
-def test_edit_host_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_edit_host_set(u):
     id = objects.add_host_set(u, "myhostset3", "ipv4", ["6.7.8.9"])
     objects.edit_host_set(u, id, "myhostset3b", "ipv4", ["1.1.1.1", "2.2.2.2"])
     assert u.get("objects", id, "name") == "myhostset3b"
     assert u.get_all("objects", id, "ipaddr") == ("1.1.1.1", "2.2.2.2")
 
-def test_delete_host_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_delete_host_set(u):
     with pytest.raises(ValidationError):
         objects.delete_host_set(u, "notpresent")
     id = objects.add_host_set(u, "myhostset4", "ipv4", ["6.7.8.9"])
     assert objects.delete_host_set(u, id) == id
 
-def test_is_used_host_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_used_host_set(u):
     id = objects.add_host_set(u, "myhostset", "ipv4", ["1.1.1.1"])
     u.set('firewall', 'r6', 'ns_src', f"objects/{id}")
     used, matches = objects.is_used_host_set(u, id)
     assert used
     assert matches == ["firewall/r6"]
 
-def test_list_host_sets(tmp_path):
-    u = _setup_db(tmp_path)
+def test_list_host_sets(u):
     sets = objects.list_host_sets(u)
     assert len(sets) == 6
 
-def test_is_used_object(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_used_object(u):
     used, matches = objects.is_used_object(u, "dhcp/ns_8dcab636")
     assert used
     assert matches == ["firewall/r5"]
     assert objects.is_used_object(u, "dhcp/ns_8bec5896")[0] == False
 
-def test_get_object(tmp_path):
-    u = _setup_db(tmp_path)
+def test_get_object(u):
     id = objects.add_host_set(u, "myhostset", "ipv4", ["1.2.3.4"])
     obj = objects.get_object(u, id)
 
-def test_get_object_ips(tmp_path):
-    u = _setup_db(tmp_path)
+def test_get_object_ips(u):
     id0 = objects.add_host_set(u, "myhostset0", "ipv4", ["4.5.6.7"])
     id = objects.add_host_set(u, "myhostset", "ipv4", ["1.2.3.4", "dhcp/ns_8bec5896", "users/ns_user1", f"objects/{id0}"])
     ips = objects.get_object_ips(u, f"objects/{id}")
     assert set(ips) == set(["1.2.3.4", "7.8.9.1", "10.10.10.22", "4.5.6.7"]) # check with set to ignore order
 
-def test_is_domain_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_domain_set(u):
     id = objects.add_domain_set(u, "mydomainset6", "ipv4", ["test1.com", "test2.com"])
     assert objects.is_domain_set(u, f"objects/{id}") == True
     assert objects.is_domain_set(u, "dhcp/ns_8dcab636") == False
     assert objects.is_domain_set(u, "users/ns_user1") == False
 
-def test_is_domain(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_domain(u):
     id = objects.add_domain_set(u, "mydomainset6", "ipv4", ["test1.com", "test2.com"])
     assert objects.is_domain(u, f"objects/{id}") == False
     assert objects.is_domain(u, "dhcp/ns_8bec5896")
 
-def test_is_host(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_host(u):
     assert objects.is_host(u, "dhcp/ns_8bec5896") == False
     assert objects.is_host(u, "dhcp/ns_8dcab636")
 
-def test_is_vpn_user(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_vpn_user(u):
     assert objects.is_vpn_user(u, "users/ns_user1")
     assert objects.is_vpn_user(u, "users/ns_user2") == False
 
-def test_is_host_set(tmp_path):
-    u = _setup_db(tmp_path)
+def test_is_host_set(u):
     id = objects.add_host_set(u, "myhostset", "ipv4", ["1.2.3.4"])
     assert objects.is_host_set(u, f"objects/{id}")
     assert objects.is_host_set(u, "dhcp/ns_8dcab636") == False
