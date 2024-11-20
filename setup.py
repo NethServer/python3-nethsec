d14b9c49bdc144197f4898c344b72932915562c7
Andrea Leardini <andrea.leardini@nethesis.it>
2024-11-20 15:56:55 +0100

    chore: version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'nethsec',
-    version = '0.0.82',
+    version = '0.0.83',
     author = 'Giacomo Sanchietti',
     author_email = 'giacomo.sanchietti@nethesis.it',
     description = 'Utilities for NethSecurity development',
