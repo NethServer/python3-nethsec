62c03700f19354c1cd711af1f6a4d0a71e15a862
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-03-12 09:39:28 +0100

    build: moving from ubuntu to python base image

--- builder/build.sh
+++ builder/build.sh
@@ -11,51 +11,8 @@
 
 set -e
 
-repobase="ghcr.io/nethserver"
-
-images=()
-container=$(buildah from docker.io/ubuntu:jammy)
-
-trap "buildah rm ${container}" EXIT
-
-echo "Disalble IPv6 for APT"
-buildah run ${container} /bin/bash -c "echo 'Acquire::ForceIPv4 \"true\";' > /etc/apt/apt.conf.d/99force-ipv4"
-
-echo "Installing build depencies..."
-buildah run ${container} /bin/bash -c "apt-get update"
-buildah run ${container} /bin/bash -c "apt install software-properties-common -y && add-apt-repository ppa:deadsnakes/ppa -y && apt-get install python3.10 -y"
-buildah run ${container} /bin/bash -c "apt-get update && apt-get -y install --no-install-recommends \
-    lua5.1 liblua5.1-0-dev libjson-c-dev ca-certificates git cmake make pkg-config gcc"
-
-echo "Compile libubox"
-buildah run ${container} /bin/bash -c "git clone git://git.openwrt.org/project/libubox.git ~/libubox && \
-  cd ~/libubox && \
-  cmake CMakeLists.txt && \
-  make install && \
-  cd .. && \
-  rm -rf libubox"
-
-echo "Compile uci"
-buildah run ${container} /bin/bash -c "git clone git://git.openwrt.org/project/uci.git ~/uci && \
-  cd ~/uci && \
-  cmake cmake CMakeLists.txt && \
-  make install && \
-  cd .. && \
-  rm -rf uci"
-
-echo "Install packages needed for PyUci"
-buildah run ${container} /bin/bash -c "apt-get -y install --no-install-recommends \
-    python3-dev python3-setuptools python3-pip lcov python3-venv && \
-    apt-get clean"
-
-echo "Install packages with pip"
-buildah run ${container} /bin/bash -c "pip install pytest==7.1.2 pyuci pytest-mock passlib"
-
-echo "Setup image"
-buildah config --workingdir /root ${container}
-buildah config --cmd='["python3", "-m", "pytest"]' ${container}
-buildah commit ${container} "${repobase}/python3-nethsec-test"
-images+=("${repobase}/python3-nethsec-test")
+podman build --force-rm --layers --jobs 0 --tag ghcr.io/nethserver/python3-nethsec-test .
+images+=("ghcr.io/nethserver/python3-nethsec-test")
 
 if [[ -n "${CI}" ]]; then
     # Set output value for Github Actions
