c9c10348b951a4f9dcabfb1c25f2cc861d72de07
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-11-26 11:23:39 +0100

    fix(firewall): added optional tags for service creation (#107)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'nethsec',
-    version = '1.4.2',
+    version = '1.4.3',
     author = 'Giacomo Sanchietti',
     author_email = 'giacomo.sanchietti@nethesis.it',
     description = 'Utilities for NethSecurity development',
